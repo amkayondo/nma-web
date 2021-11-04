c40bdb0c525a6c255febb7303ab0640767a8e42d
amkayondo <amkayondo01@gmail.com>
2021-11-05 00:08:29 +0300

    fix build error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/login': { page: '/login' },
+      '/signup': { page: '/auth/signup' },
+      '/resetPassword': { page: '/resetPassword' },
+    }
+  },
 }
