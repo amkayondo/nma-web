afb37cd6fef66805eb33faeafc582719cc16b5b0
amkayondo <amkayondo01@gmail.com>
2021-11-04 23:11:30 +0300

    add new features to app added screens - login - signup - home - reset password
    not functioning
    - reset password

--- pages/auth/LoginPage.tsx
+++ pages/auth/LoginPage.tsx
@@ -1,34 +1,65 @@
-import React from 'react'
-import AuthNavBar from '../../components/auth/AuthNavBar'
-import LoginForm from '../../components/auth/LoginForm'
-import PageHead from '../../components/PageHead'
-import PageLayOut from '../../components/PageLayOut'
+import React, { useEffect, useState } from "react";
+import { useUserObservable } from "../../application/observables/useUserObservable";
+import { useUserRepository } from "../../application/repostiories/useUserRepository";
+import AuthNavBar from "../../components/auth/AuthNavBar";
+import LoginForm from "../../components/auth/LoginForm";
+import PageHead from "../../components/PageHead";
+import PageLayOut from "../../components/PageLayOut";
+import { useObservable } from "../../infrastructure/hooks/useObservable";
+import { loginUserObject } from "../../infrastructure/types";
+import { useRouter } from "next/router";
 
-
-const LoginPage = () => {
-    return (
-      <>
-        <PageHead pageName="Login" />
-        <PageLayOut
-          fluid
-          style={{
-            backgroundColor: "#F7911D",
-            minHeight: "100%",
-            width: "100vw",
-            height: "100vh",
-          }}
-        >
-          <AuthNavBar />
-          <div className="d-flex justify-content-center">
-            <LoginForm
-              style={{
-                marginTop: "70px",
-              }}
-            />
-          </div>
-        </PageLayOut>
-      </>
-    );
+export interface loginPageProps {
+  event: {
+    preventDefault?: any;
+    onSubmit: any;
+  };
 }
 
-export default LoginPage
+const LoginPage = () => {
+  const router = useRouter();
+  const userRepository = useUserRepository();
+  const userState = useObservable(userRepository.getUserObservable());
+  const onLoginSubmit = (data: loginUserObject) => {
+    return userRepository.loginUserAction({
+      email: data.email,
+      password: data.password,
+    });
+  };
+  if (userState.isLoggedIn === true) {
+     window.location.replace("/");
+  }
+  
+  useEffect(() => {
+    userRepository.getUserProfileAction();
+  }, []);
+
+  console.log(userState);
+
+  return (
+    <>
+      <PageHead pageName="Login" />
+      <PageLayOut
+        fluid
+        style={{
+          backgroundColor: "#F7911D",
+          minHeight: "100%",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <AuthNavBar />
+        <div className="d-flex justify-content-center">
+          <LoginForm
+            style={{
+              marginTop: "70px",
+            }}
+            onSubmit={onLoginSubmit}
+          />
+        </div>
+      </PageLayOut>
+    </>
+  );
+};
+
+export default LoginPage;
